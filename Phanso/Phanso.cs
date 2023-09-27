167f3d592248d2edba1991ad115e9e9cbe2fb07e
AliceSynthesis30 <127684011+AliceSynthesis30@users.noreply.github.com>
2023-09-27 08:37:09 +0700

    Add project files.

---
+++ Phanso/Phanso.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Phanso
+{
+    internal class Phanso
+    {
+        private int iTu;
+        private int iMau;
+        public Phanso (int iTu, int iMau)
+        {
+            this.iTu = iTu; 
+            this.iMau = iMau;
+        }
+        public Phanso Cong( Phanso a)
+        {
+            int Tu =this.iTu*a.iMau+this.iMau*a.iTu;
+            int Mau = this.iMau * a.iMau;
+            return new Phanso(Tu, Mau);
+        }
+        public Phanso Tru(Phanso a)
+        {
+            int Tu = this.iTu * a.iMau - this.iMau * a.iTu;
+            int Mau = this.iMau * a.iMau;
+            return new Phanso(Tu, Mau);
+        }
+        public Phanso Nhan(Phanso a)
+        {
+            int Tu = this.iTu * a.iTu;
+            int Mau = this.iMau * a.iMau;
+            return new Phanso(Tu, Mau);
+        }
+        public Phanso Chia(Phanso a)
+        {
+            int Tu = this.iTu * a.iMau;
+            int Mau = this.iMau * a.iTu;
+            return new Phanso(Tu, Mau);
+        }
+
+    }
+}
---
+++ Phanso/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Phanso
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
